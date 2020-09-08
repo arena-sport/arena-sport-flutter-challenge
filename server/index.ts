5595bea833e9159bcd50064b27e44e866cc5ad01
me-here <mihirthanekar@gmail.com>
2020-09-07 22:21:14 -0700

    Home Screen data in graphql

--- server/index.ts
+++ server/index.ts
@@ -1,65 +1,57 @@
-export {};
+export { };
+
+import * as secrets from "./secrets.json";
 
 const { GraphQLServer } = require('graphql-yoga');
 const fetch = require('node-fetch');
 
-const options = {
+// Get type definitions
+const fs = require('fs');
+const path = require('path');
+const typeDefs = fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8");
+
+const football_api_options = {
     headers: {
         "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-        "x-rapidapi-key": "321c545a2amshe07deeb6f221be6p1bf2a3jsnd3aafcc969bf",
+        "x-rapidapi-key": secrets['API_FOOTBALL'],
         "useQueryString": true
     }
 }
 
-const typeDefs = `
-    type Query {
-        teams: [Team]
-        games: [Game]
-        notices: [Notice],
-        id: Int,
-    }
-
-    type Team {
-        team_id: Int
-        name: String
-        logo: String
-    }
-
-    type Game {
-        event_date: String
-        homeTeam: Team
-        awayTeam: Team
-        goalsHomeTeam: Int
-        goalsAwayTeam: Int
-    }
-
-    type Notice {
-        title: String
-    }
-`;
+const newsAPI = {
+    key: '18dcef7737a7410b846396edb9dc9fa2'
+}
 
 const resolvers = {
-    // Game: {
-    //     homeTeam: (parent) => {
-    //         console.log('fksjfksaj');
-    //         //console.log(parent);
-    //     }
-    // },
+    Team: {
+        name: (parent) => {
+            return parent.team_name;
+        }
+    },
     Query: {
-        id: (_) => {
-            return 24;
-        },
         teams: async (_) => {
-            const response = await fetch("https://api-football-v1.p.rapidapi.com/v2/teams/league/2", options);
+            const response = await fetch("https://api-football-v1.p.rapidapi.com/v2/teams/league/2", football_api_options);
             const parsed = await response.json();
             return parsed.api.teams;
         },
         games: async (_) => {
             const league_id = 524;
-            const response = await fetch(`https://api-football-v1.p.rapidapi.com/v2/fixtures/league/${league_id}/last/10`, options);
+            const response = await fetch(`https://api-football-v1.p.rapidapi.com/v2/fixtures/league/${league_id}/last/10`, football_api_options);
+            const parsed = await response.json();
+
+            return parsed.api.fixtures;
+        },
+        notices: async (_) => {
+            const query = 'soccer';
+            const response = await fetch(
+                `http://newsapi.org/v2/everything?q=${query}&` +
+                'from=2020-09-08&' +
+                'sortBy=popularity&' +
+                `apiKey=${secrets["NEWS_API"]}`
+            );
             const parsed = await response.json();
             
-            return parsed.api.fixtures;
+            return parsed.articles;
         }
     }
 }
